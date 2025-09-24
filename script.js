61c054eac6d4d48743f650f9ca73d9907ffe0d50
Dan Mason <dan@danmason.co.uk>
2025-09-24 19:16:22 +0100

    Initial commit with project structure

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("script.js loaded successfully!");
+
+// You can start writing your JavaScript code here.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('HTML document has been loaded and parsed.');
+});
